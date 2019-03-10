102df61e32a94d77035a798e5b915c4eb253e7e4
SnakePin <18491360+SnakePin@users.noreply.github.com>
2019-03-10 15:38:38 +0300

    Name changes and simplified code

--- AutoHeatShutdown/Form1.Designer.cs
+++ AutoHeatShutdown/Form1.Designer.cs
@@ -30,32 +30,32 @@
         {
             this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.pcShutdownTempNumeric = new System.Windows.Forms.NumericUpDown();
             this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.temperatureLabel = new System.Windows.Forms.Label();
+            this.setButton = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcShutdownTempNumeric)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 67);
+            this.label1.Location = new System.Drawing.Point(12, 93);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(160, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Cpu Package Temp : \r\n";
             // 
-            // numericUpDown1
+            // pcShutdownTempNumeric
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(168, 12);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(55, 20);
-            this.numericUpDown1.TabIndex = 1;
-            this.numericUpDown1.Value = new decimal(new int[] {
+            this.pcShutdownTempNumeric.Location = new System.Drawing.Point(168, 12);
+            this.pcShutdownTempNumeric.Name = "pcShutdownTempNumeric";
+            this.pcShutdownTempNumeric.Size = new System.Drawing.Size(55, 20);
+            this.pcShutdownTempNumeric.TabIndex = 1;
+            this.pcShutdownTempNumeric.Value = new decimal(new int[] {
             100,
             0,
             0,
@@ -65,31 +65,31 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(12, 12);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(150, 20);
             this.label2.TabIndex = 2;
             this.label2.Text = "Shutdown PC at (℃)";
             // 
-            // label3
+            // temperatureLabel
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(164, 67);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(50, 20);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "NULL";
+            this.temperatureLabel.AutoSize = true;
+            this.temperatureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.temperatureLabel.Location = new System.Drawing.Point(173, 93);
+            this.temperatureLabel.Name = "temperatureLabel";
+            this.temperatureLabel.Size = new System.Drawing.Size(50, 20);
+            this.temperatureLabel.TabIndex = 3;
+            this.temperatureLabel.Text = "NULL";
             // 
-            // button1
+            // setButton
             // 
-            this.button1.Location = new System.Drawing.Point(229, 9);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Set";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.setButton.Location = new System.Drawing.Point(229, 9);
+            this.setButton.Name = "setButton";
+            this.setButton.Size = new System.Drawing.Size(75, 23);
+            this.setButton.TabIndex = 4;
+            this.setButton.Text = "Set";
+            this.setButton.UseVisualStyleBackColor = true;
+            this.setButton.Click += new System.EventHandler(this.setButton_Click);
             // 
             // timer1
             // 
@@ -100,7 +100,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 87);
+            this.label4.Location = new System.Drawing.Point(13, 113);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(117, 13);
             this.label4.TabIndex = 5;
@@ -112,16 +112,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(355, 135);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
+            this.Controls.Add(this.setButton);
+            this.Controls.Add(this.temperatureLabel);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pcShutdownTempNumeric);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Auto Heat Shutdown";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pcShutdownTempNumeric)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -130,10 +131,10 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown pcShutdownTempNumeric;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label temperatureLabel;
+        private System.Windows.Forms.Button setButton;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label label4;
     }
--- AutoHeatShutdown/Form1.cs
+++ AutoHeatShutdown/Form1.cs
@@ -14,58 +14,50 @@ namespace AutoHeatShutdown
             InitializeComponent();
         }
 
-        int packageTemp = 0;
         int pcShutdownTemp = 100;
-
-        Computer myComputer;
+        Computer thisComputer;
+        List<int> packageTempAvarage = new List<int>();
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            myComputer = new Computer() { CPUEnabled = true };
-            myComputer.Open();
-
-
+            thisComputer = new Computer() { CPUEnabled = true };
+            thisComputer.Open();
         }
-        List<int> packageTempAvarage = new List<int>();
+
         private void timer1_Tick(object sender, EventArgs e)
         {
-            foreach (var hardwareItem in myComputer.Hardware)
-            {
-                if (hardwareItem.HardwareType == HardwareType.CPU)
-                {
-                    hardwareItem.Update();
+            IHardware hardwareItem = thisComputer.Hardware.FirstOrDefault(x => x.HardwareType == HardwareType.CPU);
+            hardwareItem.Update();
 
-                    foreach (var sensor in hardwareItem.Sensors)
-                    {
-                        if (sensor.SensorType == SensorType.Temperature && sensor.Name == "CPU Package")
-                        {
-                            packageTemp = (int)sensor.Value.Value;
-
-                        }
-                    }
-                }
-            }
+            int packageTemp = (int)hardwareItem.Sensors.FirstOrDefault(x => x.SensorType == SensorType.Temperature && x.Name == "CPU Package").Value.Value;
 
             packageTempAvarage.Add(packageTemp);
 
-            if(packageTempAvarage.Count > 10)
-                packageTempAvarage.RemoveAt(0);
-
-            label3.Text = ((int)packageTempAvarage.Average()).ToString() + "℃";
-
-            if ((int)packageTempAvarage.Average() > pcShutdownTemp && packageTempAvarage.Count >= 9)
+            if (packageTempAvarage.Count > 10)
             {
-                var psi = new ProcessStartInfo("shutdown", "/s /t 0");
-                psi.CreateNoWindow = true;
-                psi.UseShellExecute = false;
-                Process.Start(psi);
+                while (packageTempAvarage.Count != 10)
+                {
+                    packageTempAvarage.RemoveAt(0);
+                }
+            }
+
+            temperatureLabel.Text = ((int)packageTempAvarage.Average()).ToString() + "℃";
+
+            if ((int)packageTempAvarage.Average() >= pcShutdownTemp && packageTempAvarage.Count == 10)
+            {
                 Console.WriteLine("Auto shutdown commencing..");
+                ProcessStartInfo psi = new ProcessStartInfo("shutdown", "/s /t 0")
+                {
+                    CreateNoWindow = true,
+                    UseShellExecute = false
+                };
+                Process.Start(psi);
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void setButton_Click(object sender, EventArgs e)
         {
-            pcShutdownTemp = (int)numericUpDown1.Value;
+            pcShutdownTemp = (int)pcShutdownTempNumeric.Value;
         }
     }
 }
